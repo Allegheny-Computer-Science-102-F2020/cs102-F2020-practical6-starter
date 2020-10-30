b060fb9fda76a158b3e47ac84259b1ac3985f872
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-10-30 06:58:37 -0400

    Add the template for the test case to the test_collatz test suite.

---
+++ collatzcreator/tests/test_collatz.py
@@ -0,0 +1,14 @@
+"""Add test cases for the function in the collatz module."""
+
+
+from collatzcreator import collatz
+
+
+def test_collatz_input_three():
+    """Ensure that input of the number 3 produces the sequence suggested by Stavely on page 92."""
+    number = 3
+    # TODO: create a generator function of all of the outputs
+    # TODO: materialize a list from the generator function, which
+    # will support multiple assertions on the list
+    # TODO: ensure that there are eight values in the list
+    # TODO: confirm that the contents of the list are correct
