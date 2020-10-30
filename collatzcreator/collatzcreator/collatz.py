8715aba971efe2a1cb40708f5115cbe9841c2c29
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-10-30 07:01:51 -0400

    Add the starting version of the collatz module with Collatz function.

---
+++ collatzcreator/collatzcreator/collatz.py
@@ -0,0 +1,17 @@
+"""Compute values in the Fibonacci sequence using different approaches."""
+
+from typing import Iterator
+
+
+def Collatz(number: int) -> Iterator[int]:
+    """Compute the numbers in the Collatz sequence for the starting number."""
+    # TODO: yield the provided number
+    # TODO: continue to iterate until the number is equal to 1
+    # NOTE: there is no proof that this function will stop running!
+    # NOTE: can you provide an answer to the following problem?
+    # Reference:
+    # https://projecteuler.net/problem=14
+    #
+    # TODO: make sure to perform integer division by two
+    # Reference:
+    # https://stackoverflow.com/questions/183853/what-is-the-difference-between-and-when-used-for-division
